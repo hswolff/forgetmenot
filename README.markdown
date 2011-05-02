579a4d7f2523f8cdf7d7003a297e50371a371ead
Harry Wolff <hswolff@gmail.com>
2011-05-02 14:21:19 -0400

    first commit

---
+++ README.markdown
@@ -0,0 +1,41 @@
+Forgetmenot
+===========
+
+**Forgetmenot is under development at the moment...**
+
+One-Sentence Pitch
+------------------
+Forgetmenot is *the* self-hosted ToDo List application, just as (I would argue) WordPress is *the* self-hosted blogging application.  
+
+Goal
+----
+Forgetmenot is a self-hosted ToDo List application that is accessible via web and mobile browser.  Its datastore can make use of localStorage or a self-hosted SQLite database.
+
+Features
+--------
+*  Self-hosted (who wants to rely on a 3rd-party vendor?)
+*  Accessible via modern web browser
+*  Accessible via modern mobile browsers
+*  Uses HTML5's localStorage API or a local SQLite DB
+*  Pretty to look at, pretty to use
+	* [Eventually] Skin-able
+*  Folder/Sub-list Support
+*  Meta-Information Available for each ToDo
+	*  Notes
+	*  URL
+	*  Due Date
+*  Multi-user accounts
+
+
+Dependencies
+------------
+*  Backbone.js
+*  Underscore.js
+*  jQuery
+*  jQuery Mobile
+
+
+Credits
+-------
+Thank you Jérôme Gravel-Niquet for your excellent [ToDo List example application](http://documentcloud.github.com/backbone/examples/todos/index.html)
+
---
+++ forgetmenot.js
@@ -0,0 +1,108 @@
+$(document).ready(function() {
+    
+    window.Todo = Backbone.Model.extend({
+        //Default Attributes
+        defaults: {
+            content: "new empty todo",
+            parent: 0,
+            folder: false,
+            order: 0,
+            done: false
+        },
+        
+        initialize: function() {
+            if (!this.get("content")) {
+              this.set({"content": this.defaults.content});
+            }
+        }
+    });
+    
+    window.TodoList = Backbone.Collection.extend({
+        model: Todo,
+        localStorage: new Store("forgetmenot")
+    });
+    
+    window.Todos = new TodoList;
+
+
+    window.TodoView = Backbone.View.extend({
+        tagName: "li",
+        id: "todo-",
+        className: "todo",
+        template: _.template($('#item-template').html()),
+        
+        events: {
+            "click"                 :       "edit",
+            "click div.save"        :       "save",
+            "click div.delete"      :       "delete"
+        },
+        
+        initialize: function() {
+            _.bindAll(this, 'render', 'delete');
+            this.render();
+        },
+        
+        render: function() {
+            $(this.el).html(this.template(this.model.toJSON()));
+            return this;
+        },
+        
+        edit: function() {
+            this.$("span").hide();
+            this.$("input, div").show();
+            //return false;
+        },
+        
+        save: function() {
+            var newVal = this.$("input").val();
+            this.model.save({content: newVal});
+            this.$("span").show();
+            this.$("input, div").hide();
+            this.$("span").text(newVal);
+        },
+        
+        delete: function() {
+            this.model.destroy();
+            this.remove();
+        }
+    });
+
+    //window.todoview = new TodoView({model: todo});
+
+    window.TodoApp = Backbone.View.extend({
+        el: $("#todoApp"),
+        
+        events: {
+            //"dblclick"          :       "test",
+            "click #createNew"  :       "createNew"
+        },
+        
+        initialize: function() {
+            _.bindAll(this, 'createNew', 'addOne', 'addAll');
+            Todos.bind('refresh', this.addAll);
+            Todos.fetch();
+        },
+        
+        createNew: function() {
+            var todo = Todos.create();
+            var view = new TodoView({model:todo});
+            $(this.el).append(view.render().el);
+        },
+        
+        addOne: function(todo) {
+            var view = new TodoView({model:todo});
+            $(this.el).append(view.render().el);
+        },
+        
+        addAll: function() {
+            Todos.each(this.addOne);
+        },
+        
+        test: function() {
+            alert('test');
+        }
+    });
+
+    window.todoapp = new TodoApp;
+
+});
