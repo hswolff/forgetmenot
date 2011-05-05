9821ccd6c4d2def3bb6f7e739c60c851c33a4924
Harry Wolff <hswolff@gmail.com>
2011-05-05 13:16:26 -0400

    make sure each todo list item has a unique ID in the DOM

--- forgetmenot.js
+++ forgetmenot.js
@@ -27,7 +27,7 @@ $(document).ready(function() {
 
     window.TodoView = Backbone.View.extend({
         tagName: "li",
-        id: "todo-" + this.model,
+        id: "todo-",
         className: "todo",
         template: _.template($('#item-template').html()),
         
@@ -40,6 +40,7 @@ $(document).ready(function() {
         
         initialize: function() {
             _.bindAll(this, 'render', 'edit', 'deleteTodo', 'save', 'updateOnEnter');
+            this.el.id += this.model.get("id");
             this.render();
         },
         
