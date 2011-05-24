8a7606cf0e1faf96fd02fbb9cfb1660e2bee2dc6
Harry Wolff <hswolff@gmail.com>
2011-05-23 22:58:30 -0400

    change save event to focus out

--- forgetmenot.js
+++ forgetmenot.js
@@ -49,7 +49,7 @@ $(document).ready(function() {
         render: function() {
             $(this.el).html(this.template(this.model.toJSON()));
             this.input = this.$("input");
-            this.input.bind('blur', this.save);
+            this.input.bind('focusout', this.save);
             return this;
         },
         
