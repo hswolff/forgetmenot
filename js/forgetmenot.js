321aac3dec9ee99a98096db727d55537c058923a
Harry Wolff <hswolff@gmail.com>
2011-07-20 22:06:27 -0400

    first rehaul work done.
    working for the most part

--- js/forgetmenot.js
+++ js/forgetmenot.js
@@ -1,6 +1,8 @@
-$(document).ready(function() {
-    
-    window.Todo = Backbone.Model.extend({
+$(function() {
+    // Global namespace
+	window.fmn = {};
+	
+    fmn.Model = Backbone.Model.extend({
         //Default Attributes
         defaults: {
             content: "new empty todo",
@@ -24,111 +26,8 @@ $(document).ready(function() {
 		    }
 			
 		},
-		
-		setParent: function(parentTodo) {
-			if(!parentTodo) {
-				return false;
-			}
-			this.save({parent: parentTodo.get('id')});
-			return true;
-		},
-		
-		indentBy: function(number) {
-			this.save({indent: (this.get('indent') + (number ? number: 0))});
-		}
-    });
-    
-    window.TodoList = Backbone.Collection.extend({
-        model: Todo,
-//        localStorage: new Store("forgetmenot"),
-		url: "fmn.php?",
-        
-        nextPosition: function() {
-            if (this.length === 0) {
-                return 1;
-            } else {
-                return this.last().get('position') + 1;
-            }
-        },
 
-		thoseWithParentOf: function(parentId) {
-			return _.select(this.models, function(model) {
-				return model.get('parent') == parentId;
-			});
-		},
-		
-		thatAreDone: function() {
-			return _.select(this.models, function(model) {
-				return model.get('done') == true;
-			});
-		},
-        
-        comparator: function(todo) {
-          return todo.get('position');
-        },
-		
-		// Get's passed current todo
-		// Returns parent todo
-		// Checking to make sure the
-		// correct parent todo is returned
-		getParentTodo: function(todo, previousTodo) {
-			var previousTodo = this.at(Todos.indexOf(previousTodo ? previousTodo : todo) - 1);
-			if (!previousTodo) {
-				return false;
-			} else if (todo.get('indent') == previousTodo.get('indent')) {
-				return (previousTodo ? previousTodo : NaN);
-			} else {
-				return this.getParentTodo(todo, previousTodo);
-			}
-			
-		},
-		
-		maintainChildrenOfParent: function(todo) {
-			var childTodo = this.at(Todos.indexOf(childTodo ? childTodo: todo) + 1);
-			if (!childTodo) {
-				return false;
-			} else {
-				if(todo.get('indent') == 0 && todo.get('id') == this.getNextTodo(todo).get('parent')) {
-					this.getNextTodo(todo).save({parent: todo.get('id'), indent: 1});
-				}
-				Todos.each(function(model){
-					if (todo.get('id') === model.get('parent')) {
-						model.save({indent: (todo.get('indent') + 1)});
-					}
-				});
-				return this.maintainChildrenOfParent(childTodo);
-			}
-			
-		},
-		
-		// Gets passed current todo
-		// Sets current todo's parent todo Id correctly
-		setParentTodo: function(todo) {
-			todo.get('position');
-			var previousTodo = this.getPreviousTodo(todo);
-			if (previousTodo.get('parent') == '0') {
-				todo.setParent(previousTodo);
-			}
-		},
-		
-		// Passed in current todo
-		// Returns next todo item
-		// Based off of todo's 'position' attribute
-		getNextTodo: function(todo) {
-			return this.at(Todos.indexOf(todo) + 1);
-		},
-		
-		// Passed in current todo
-		// Returns previous todo item
-		// Based off of todo's 'position' attribute
-		getPreviousTodo: function(todo) {
-			return this.at(Todos.indexOf(todo) - 1);
-		}
-		
     });
-    
-    window.Todos = new TodoList;
-
 
     // Each Individual Todo Item View
     window.TodoView = Backbone.View.extend({
@@ -197,67 +96,21 @@ $(document).ready(function() {
 			**/
 			// Enter button
 			if (e.keyCode == 13) {
-				if(!Todos.getNextTodo(this.model)) {
-					// If last todo then close current 
-					// and create a new todo
-					this.close();
-					forgetmenot.createNewAfter(this.model);
-				} else if (this.model.get('indent') === Todos.getNextTodo(this.model).get('indent')) {
-					// If next todo is at same indent
-					// Then edit next todo
-					this.editNextTodo(e);
-				} else if (this.model.get('indent') > Todos.getNextTodo(this.model).get('indent')) {
-					// If next todo is closer to indent '0'
-					// Then we create a new todo after current
-					forgetmenot.createNewAfter(this.model);
-				} else {
-				// If not last todo then edit next todo
-					this.editNextTodo(e);
-				}
+				this.close();
+				fmn.app.newTodo();
+				e.preventDefault();
 			}
 			// Tab key - move todo to right one
 			// And make sub todo of parent (if it exists)
 			if (e.keyCode == 9 && !e.shiftKey) {
-				/* disable for now
-				this.save();
-				if(this.model.setParent(Todos.getParentTodo(this.model))) {
-					this.save(1);
-					Todos.maintainChildrenOfParent(this.model);
-					this.edit();
-				}
-				*/
 				e.preventDefault();
 			}
 			// Shift + Tab key - move todo to left one
 			// And un-sub it from parent (if it exists)
 			if (e.shiftKey && e.keyCode == 9) {
-				/* disable for now
-				this.save();
-				if(this.model.get('indent') == 1) {
-					this.model.save({
-						indent: 0,
-						parent: 0
-					});
-					Todos.maintainChildrenOfParent(this.model);
-				} else if (!Todos.getPreviousTodo(this.model) || this.model.get('indent') == 0) {
-					return false;
-				} else {
-					this.model.save({
-						parent: Todos.get(this.model.get('parent')).get('parent'),
-						indent: Todos.get(this.model.get('parent')).get('indent')
-					});
-					Todos.maintainChildrenOfParent(this.model);
-				}
-				this.edit();
-				*/
 				e.preventDefault();
 			}
-			/**
-				Note for Up and Down keys:
-				The '- 2' and none increments are
-				due to the collection being kept at index 0
-				but the position attribute being kept at index 1
-			**/
+
 			// Up key - close current todo and open todo above to edit
 			if (e.keyCode == 38) {
 				this.editPreviousTodo(e);
@@ -276,119 +129,57 @@ $(document).ready(function() {
 			}
         },
 
-		editNextTodo: function(e) {
-			var todoToOpen = Todos.getNextTodo(this.model);
-			// If we're already at the top then preventDefault()
-			if (!todoToOpen) {
-				e.preventDefault();
-			} else {
-				this.close();
-				todoToOpen.view.edit();
-			}
-		},
-		
-		editPreviousTodo: function(e) {
-			var todoToOpen = Todos.getPreviousTodo(this.model);
-			// If we're already at the top then preventDefault()
-			if (!todoToOpen) {
-				e.preventDefault();
-			} else {
-				this.close();
-				todoToOpen.view.edit();
-			}
-		},
-
 		toggleDone: function() {
 			this.model.done();
 		}
     });
 
-    window.TodoApp = Backbone.View.extend({
+    fmn.Collection = Backbone.Collection.extend({
+        model: fmn.Model,
+		url: "fmn.php?",
+        
+        comparator: function(todo) {
+          return todo.get('id');
+        }
+    });
+    
+    fmn.Todos = new fmn.Collection;
+
+    fmn.App = Backbone.View.extend({
         el: $("#todoApp"),
         
         events: {
-            //"dblclick"          :       "test",
-            "click #createNew"  :       "createNew"
+            "click #createNew"  :       "newTodo"
         },
         
         initialize: function() {
-//            Backbone.emulateHTTP = true;
             Backbone.emulateJSON = true;
-            _.bindAll(this, 'createNew', 'createNewAfter', 'createNewBefore', 'addOne', 'addAll');
-            Todos.bind('reset', this.addAll);
-            //Todos.bind('refresh', this.resetPosition);
-			//Todos.bind('remove', this.resetPosition);
-			//Todos.bind('add', this.resetPosition);
-            Todos.fetch({success: function(){
-				if (Todos.length == 0) {
-					forgetmenot.createNew({content: 'Start your first Todo!', topOrBottom: 'top'});
-				}
-			}});
-			this.resetPosition();
-			
-        },
-        
-        createNew: function(o) {
-            var todo = Todos.create({
-				content: o.content ? o.content : ''
-			});
-            this.addOne(todo, (_.isString(o.topOrBottom) ? o.topOrBottom : 'bottom')).edit();
+            _.bindAll(this, 'newTodo', 'addOne', 'addAll');
+            fmn.Todos.bind('reset', this.addAll);
+			fmn.Todos.bind('add', this.addOne);
+            fmn.Todos.fetch();
         },
 
-		createNewAfter: function(todo) {
-            var t = Todos.create({
-				content: '',
-				parent: todo.get('parent'),
-				indent: parseInt(todo.get('indent')),
-				position: parseInt(todo.get('position')) + 1
-			});
-			var view = new TodoView({model:t});
-			this.$('#' + todo.cid).after(view.render().el);
-			view.edit();
-        },
-
-		createNewBefore: function(todo) {
-            var t = Todos.create({
-				content: '',
-				parent: todo.get('parent'),
-				indent: parseInt(todo.get('indent')),
-				position: todo.get('position')
-			});
-			var view = new TodoView({model:t});
-			this.$('#' + todo.id).before(view.render().el);
-			view.edit();
-        },
-        
-        addOne: function(todo, topOrBottom) {
-            var view = new TodoView({model:todo});
-			if (topOrBottom == 'top') {
-				this.$("#todoItemsList").prepend(view.render().el);
-			} else if (topOrBottom == 'bottom') {
-				var lastTodoPosition = Todos.last().get("position") + 1;
-				todo.set({position: lastTodoPosition});
-				Todos.sort({silent: true});
-				this.$("#todoItemsList").append(view.render().el);
-			} else {
-				this.$("#todoItemsList").append(view.render().el);
+        addAll: function(collection, r) {
+			// If collection is empty add one
+			if(!collection.length) {
+				var model = collection.create();
 			}
+            collection.each(this.addOne);
+        },
+        
+        addOne: function(o, p) {
+            var view = new TodoView({model:o.todo || o});
+			this.$("#todoItemsList").append(view.render().el);
             return view;
         },
-        
-        addAll: function() {
-            Todos.each(this.addOne);
-        },
 
-        resetPosition: function(todo) {
-            var position = -1;
-            _.each(Todos.models, function(todo) {
-                position++;
-                todo.set({ position: position});
-                todo.save();
-            });
-            //Todos.refresh();
+        newTodo: function(o) {
+            fmn.Todos.create();
         }
+
     });
 
-    window.forgetmenot = new TodoApp;
+    fmn.app = new fmn.App;
 
 });
