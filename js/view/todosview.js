f5813872564b0e5a2a1e6451c2f6fb950d20c68e
Harry Wolff <hswolff@gmail.com>
2012-02-19 00:33:27 -0500

    cleanup collection view

--- js/view/todosview.js
+++ js/view/todosview.js
@@ -12,22 +12,18 @@ function($, _, Backbone, Todos, TodoView) {
         initialize: function(collection) {
         	_.bindAll(this);
 
-        	this.addAll(collection);
+        	// this.addAll(collection);
+            this.$el.empty();
+            // If collection is empty add one
+            if(!collection.length) {
+                collection.create();
+            }
+            collection.each(this.addOne);
             
-            collection.bind('reset', this.addAll);
 			collection.bind('add', this.addOne);
             collection.bind('add', function(model, collection){
                 model.view.edit();
             });
-        },
-
-        addAll: function(collection, r) {
-        	this.$el.empty();
-			// If collection is empty add one
-			if(!collection.length) {
-				var model = collection.create();
-			}
-            collection.each(this.addOne);
         },
         
         addOne: function(o, p) {
